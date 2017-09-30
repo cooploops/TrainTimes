e23a90193be583fbb85cb0235a03a95d9e0cc39e
TJ <cooploops@gmail.com>
2017-09-29 23:21:20 -0700

    add html and work on javascript time conversions

--- assets/javascript/traintimes.js
+++ assets/javascript/traintimes.js
@@ -1,29 +1,69 @@
-  // Initialize Firebase
-  var config = {
-    apiKey: "AIzaSyDqeua3U0m4B5QtbGZod07AsNxK35zHQ0U",
-    authDomain: "my-awesome-project-c1de8.firebaseapp.com",
-    databaseURL: "https://my-awesome-project-c1de8.firebaseio.com",
-    projectId: "my-awesome-project-c1de8",
-    storageBucket: "my-awesome-project-c1de8.appspot.com",
-    messagingSenderId: "968353723398"
-  };
-  firebase.initializeApp(config);
+// Initialize Firebase
+var config = {
+  apiKey: "AIzaSyAbRL9IUBZ-Xr1Q15HbV5x_0RvseK_VmZE",
+  authDomain: "traintimes-c310d.firebaseapp.com",
+  databaseURL: "https://traintimes-c310d.firebaseio.com",
+  projectId: "traintimes-c310d",
+  storageBucket: "",
+  messagingSenderId: "426995379590"
+};
+firebase.initializeApp(config);
 
-  var database = firebase.database();
+var database = firebase.database();
 
   window.onload = function(){
 
+    // create object to hold data to be stored
     var trainData = {
       trainName: "",
-      dest: "",
+      destination: "",
       firstDepart: 0,
       freq: 0
     }
-
-    $("#addTrain").on("click", function(event) {
+    // when addTrain button is pushed it grabs the values from the input boxes and stores them in the database at the Root
+    $("#addTrain").on("click", function(event){
       event.preventDefault();
 
+      trainData.trainName = $("#trainName").val().trim();
+      trainData.destination = $("#destination").val().trim();
+      trainData.firstDepart = $("#firstDepart").val().trim();
+      trainData.freq = $("#freq").val().trim();
+
+      database.ref().push({trainData});
+      // clear input boxes after submission made
+      $("#trainName").val("");
+      $("#destination").val("");
+      $("#firstDepart").val("");
+      $("#freq").val("");      
       
-    }
+    });
 
   }
+
+database.ref().on("child_added", function(snapshot){
+  console.log(snapshot.val().trainData.destination);
+
+  var trainName = snapshot.val().trainData.trainName;
+  var destination = snapshot.val().trainData.destination;
+  var firstDepart = snapshot.val().trainData.firstDepart;
+  var freq = snapshot.val().trainData.freq;
+
+  var timeNow = moment();
+  console.log(moment(timeNow).format("X"));
+  var timeDepart= moment(firstDepart,"HH:mm");
+  console.log(moment(timeDepart).format("X"));
+  if(moment(timeNow).format("X") >= moment(timeDepart).format("X")){
+    var diff = timeNow.diff(timeDepart, "X");
+  } else {
+    var diff = timeDepart.diff(timeNow, "X")
+  }
+  console.log(diff);
+  var freqMoment = moment(freq,"mm").format("X");
+  console.log(freqMoment);
+  
+
+  // $("#trainTable > tbody").append(
+  //   "<tr><td> + "
+  //   )
+
+});
